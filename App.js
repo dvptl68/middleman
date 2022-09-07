5d6f828d656479094d0b6ccbf027eb8e2a95e0b9
Karthick Sivasubramanian <karthicksiva@MacBook-Pro-6.local>
2022-09-07 18:42:49 -0400

    added margins

--- App.js
+++ App.js
@@ -5,31 +5,42 @@ function Box({ children, ...props }) {
   return <View {...props}>{children}</View>
 }
 
+function padding(a, b, c, d) {
+  return {
+    paddingTop: a,
+    paddingRight: b ? b : a,
+    paddingBottom: c ? c : a,
+    paddingLeft: d ? d : (b ? b : a)
+  }
+}
+
 const HelloWorldApp = () => {
   return (
 
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+        //justifyContent: "center",
+        alignItems: "center",
+        // justifyContent: 'space-between'
       }}>
-      <Text style = {{fontSize: 30}}>Henry Cavill</Text>
+      <Text style = {{margin: 20,  fontSize: 30}}>Henry Cavill</Text>
       <Image
-        style = {{width: 200, height: 200}}
+        style = {{margin: 20, width: 200, height: 200}}
         source = {require('./assets/cavill.jpg')}
       />
       <Box
       style={{
         backgroundColor: '#333',
         borderRadius: 4,
-        color: '#eee',
+        color: '#000000',
         minHeight: 200,
         padding: 12,
         width: 300,
+        margin: 20
       }}
     >
-      <Text>Karthick Sivasubramanian</Text>
+      <Text style = {{color: "white"}}>Karthick Sivasubramanian</Text>
     </Box>
     </View>
   
