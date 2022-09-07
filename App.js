828f5ae70ae1f1eae4dfeb4096bf96672e7b011d
Karthick Sivasubramanian <karthicksiva@MacBook-Pro-6.local>
2022-09-07 17:51:36 -0400

    added box

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+
+function Box({ children, ...props }) {
+  return <View {...props}>{children}</View>
+}
 
 const HelloWorldApp = () => {
   return (
+
     <View
       style={{
         flex: 1,
@@ -10,7 +15,25 @@ const HelloWorldApp = () => {
         alignItems: "center"
       }}>
       <Text>Hello, world!</Text>
+      <Text style = {{fontSize: 30}}>Henry Cavill</Text>
+      <Image
+        style = {{width: 200, height: 200}}
+        source = {require('./assets/cavill.jpg')}
+      />
+      <Box
+      style={{
+        backgroundColor: '#333',
+        borderRadius: 4,
+        color: '#eee',
+        minHeight: 200,
+        padding: 12,
+        width: 300,
+      }}
+    >
+      <Text>Karthick Sivasubramanian</Text>
+    </Box>
     </View>
+  
   )
 }
 export default HelloWorldApp;
