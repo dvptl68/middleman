5c0583e0ebcbec0a92ee23e94fb47ca8e7a307e2
smerapala <smerapala@gmail.com>
2022-09-21 12:01:16 -0400

    Add button and supportfor file input

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,6 +5,29 @@ const Profile = () => {
   return <></>;
 };
 
+// const [data,setData]=useState([]);
+
+// const getData=()=>{
+//   fetch('assets/input_profiles/profiles.json'
+//   ,{
+//     headers : { 
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json'
+//      }
+//   }
+//   )
+//     .then(function(response){
+//       console.log(response)
+//       return response.json();
+//     })
+//     .then(function(myJson) {
+//       console.log(myJson);
+//     });
+// }
+// useEffect(()=>{
+//   getData()
+// },[])
+
 // const styles = StyleSheet.create({});
 
 export default Profile;
--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,14 +1,72 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import Profile from './../components/Profile';
+import { ProfileType } from './../components/ProfileType';
+
+
+const [currentProfileIndex, setCurrentProfileIndex] = useState(0)
 
 const Discover = () => {
   return (
     <View style={styles.page}>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        onChangeText={onChangeText}
+        clearTextOnFocus={true}
+        placeholder="Discover"
+      />
       <Profile />
+      <View>
+        <Button 
+          title= "Press me" 
+          onPress={() => setCurrentProfileIndex(currentProfileIndex == profiles.length - 1 ? 
+              0 : 
+              currentProfileIndex + 1
+          )}
+        />
+      </View>
+      <View>
+        {
+        profiles[currentProfileIndex] &&
+          <Image
+            key={profiles[currentProfileIndex].id}
+            style = {{
+              width: 300,
+              height: 300,
+            }}
+            source={profiles[currentProfileIndex].actualimage} 
+          />
+        }
+      </View>
     </View>
+
+    
   );
 };
+
+// const profiles = require('./assets/input_profiles/profiles.json');
+const profiles = ProfileType [
+  {
+    id: "1",
+    actualimage: require("./images/image1.jpeg"),
+  },
+  {
+    id: "2",
+    actualimage: require("./images/image2.jpg"),
+  },
+  {
+    id: "3",
+    actualimage: require("./images/image3.jpg"),
+  },
+  {
+    id: "4",
+    actualimage: require("./images/image4.jpg"),
+  },
+];
 
 const styles = StyleSheet.create({
   page: {
