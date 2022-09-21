152f3e1e000e4d7210e59d1893598dc7eae6f828
John Wu <jhnwu3@gmail.com>
2022-09-21 18:01:38 -0400

    flaskAPI initially setup

---
+++ data/process.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sys
+import names
+import numpy as np
+import json
+import ast
+
+f = open('data.json')
+data = json.load(f)
+
+# hypothetically somehow extract orientation + gender, + age
+
+print(data)
+
