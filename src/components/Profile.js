4f15ae6d0b71889aa1507171281d642923156d51
Dev Patel <dvptl68@gmail.com>
2022-09-21 22:53:21 -0400

    Add linting

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text } from 'react-native';
 
 const Profile = () => {
-  return (
-    <Text>Open up App.js to start working on your app!</Text>
-  );
+  return <Text>Open up App.js to start working on your app!</Text>;
 };
 
 export default Profile;
