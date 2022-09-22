8b4fcabbbae4932a984293aac866159d5d7506f6
Dev Patel <dvptl68@gmail.com>
2022-09-22 10:47:49 -0400

    Complete profile view

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -3,19 +3,39 @@ import { View, StyleSheet } from 'react-native';
 import Profile from './../components/Profile';
 
 const Discover = () => {
+  const profile = {
+    name: 'Andrew',
+    age: 24,
+    gender: 'Male',
+    height: 69,
+    location: 'Columbus',
+    sexual_orientation: 'Straight',
+    word1: 'extrovert',
+    word2: 'soccer',
+    word3: 'marvel',
+  };
   return (
-    <View style={styles.container}>
-      <Profile />
-    </View>
+    <>
+      <View style={styles.profileContainer}>
+        <Profile {...profile} />
+      </View>
+      <View style={styles.buttonsContainer}>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  profileContainer: {
+    flex: 3,
     borderWidth: 5,
     borderColor: 'red',
   },
+  buttonsContainer: {
+    flex: 1,
+    borderWidth: 5,
+    borderColor: 'blue',
+  }
 });
 
 export default Discover;
