05f34ad69d1305e1bc881099152c76d66154c75a
Dev Patel <dvptl68@gmail.com>
2022-09-22 12:30:09 -0400

    Implement profile changing

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,60 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import Profile from './../components/Profile';
 
 const Discover = () => {
-  const profile = {
-    name: 'Andrew',
-    age: 24,
-    gender: 'Male',
-    height: 69,
-    location: 'Columbus',
-    sexual_orientation: 'Straight',
-    word1: 'extrovert',
-    word2: 'soccer',
-    word3: 'marvel',
+  const [profiles, setProfile] = useState(
+    require('./../../assets/input_profiles/profiles.json')
+  );
+  const popProfile = () => {
+    setProfile((profiles) => profiles.slice(1));
   };
-  return (
+  const displayProfile = <Profile {...profiles[0]} />;
+  return profiles.length > 0 ? (
     <>
-      <View style={styles.profileContainer}>
-        <Profile {...profile} />
-      </View>
+      <View style={styles.profileContainer}>{displayProfile}</View>
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={styles.singleButtonContainer}>
-          <Image style={styles.buttons} source={require('./../../assets/images/broken-heart.png')} />
+        <TouchableOpacity
+          style={styles.singleButtonContainer}
+          onPress={popProfile}
+        >
+          <Image
+            style={styles.buttons}
+            source={require('./../../assets/images/broken-heart.png')}
+          />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.singleButtonContainer}>
-          <Image style={styles.buttons} source={require('./../../assets/images/heart.png')} />
+        <TouchableOpacity
+          style={styles.singleButtonContainer}
+          onPress={popProfile}
+        >
+          <Image
+            style={styles.buttons}
+            source={require('./../../assets/images/heart.png')}
+          />
         </TouchableOpacity>
       </View>
     </>
+  ) : (
+    <View style={styles.profileContainer}></View>
   );
 };
 
 const styles = StyleSheet.create({
   profileContainer: {
     flex: 3,
-    borderWidth: 5,
-    borderColor: 'red',
+    padding: 5,
+    // borderWidth: 5,
+    // borderColor: 'red',
   },
   buttonsContainer: {
     flex: 1,
     flexDirection: 'row',
-    borderWidth: 5,
-    borderColor: 'blue',
+    // borderWidth: 5,
+    // borderColor: 'blue',
   },
   singleButtonContainer: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 5,
-    borderColor: 'green',
+    // borderWidth: 5,
+    // borderColor: 'green',
   },
   buttons: {
     width: 75,
     height: 75,
     resizeMode: 'stretch',
-  }
+  },
 });
 
 export default Discover;
