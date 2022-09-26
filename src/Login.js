18d04e3c3c828ae96996b70b2c9130c20ef60c06
Dev Patel <dvptl68@gmail.com>
2022-09-25 21:09:07 -0400

    Add password text box

--- src/Login.js
+++ src/Login.js
@@ -4,6 +4,7 @@ import Tabs from './Tabs';
 
 const Login = () => {
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return isLoggedIn ? (
     <Tabs username={username} />
@@ -14,6 +15,13 @@ const Login = () => {
         onChangeText={setUsername}
         value={username}
         placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Password"
+        secureTextEntry={true}
       />
       <Button title="login" onPress={() => setIsLoggedIn(true)} />
     </View>
@@ -31,7 +39,7 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     width: 200,
-    margin: 12,
+    margin: 5,
     borderWidth: 1,
     padding: 10,
   },
