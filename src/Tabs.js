d58db126e69a5d7813dc4c32b7da11460167548e
Dev Patel <dvptl68@gmail.com>
2022-09-25 20:57:05 -0400

    Lint code

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,31 +1,34 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import Discover from './pages/Discover';
 import Matchmaking from './pages/Matchmaking';
 import Chat from './pages/Chat';
 import Profile from './pages/Profile';
 
-const Tabs = (props) => {
-  const [tabs] = useState([<Discover />, <Matchmaking />, <Chat />, <Profile />]);
+const Tabs = () => {
+  const [tabs] = useState([
+    <Discover key={1} />,
+    <Matchmaking key={2} />,
+    <Chat key={3} />,
+    <Profile key={4} />,
+  ]);
   const [activeTab, setActiveTab] = useState(0);
   return (
     <>
-      <View style={styles.pageContainer}>
-        {tabs[activeTab]}
-      </View>
+      <View style={styles.pageContainer}>{tabs[activeTab]}</View>
       <View style={styles.tabsContainer}>
         {['discover', 'matchmaking', 'chat', 'profile'].map((tabName, i) => {
           return (
-              <TouchableOpacity
-                key={i}
-                style={styles.singleTabContainer}
-                onPress={() => setActiveTab(i)}
-              >
-                <Image
-                  style={styles.buttons}
-                  source={require(`./../assets/images/${tabName}.png`)}
-                />
-              </TouchableOpacity>
+            <TouchableOpacity
+              key={i}
+              style={styles.singleTabContainer}
+              onPress={() => setActiveTab(i)}
+            >
+              <Image
+                style={styles.buttons}
+                source={require(`./../assets/images/${tabName}.png`)}
+              />
+            </TouchableOpacity>
           );
         })}
       </View>
