2f02b83bb1b91a7983f48ce64df061d32108407f
Dev Patel <dvptl68@gmail.com>
2022-09-28 16:55:32 -0400

    fix this dumbass shit

--- src/pages/Matchmaking.js
+++ src/pages/Matchmaking.js
@@ -6,8 +6,16 @@ import { MatchmakingStyles } from '../styles/Styles';
 const Matchmaking = (props) => {
   const [profiles, setProfiles] = useState(props.userData[props.username].mProfiles);
   const likeProfile = (liked) => {
+    const username = profiles[0];
     const newUserData = {...props.userData};
     newUserData[props.username].mProfiles = profiles.slice(1);
+    if (liked) {
+      newUserData[props.username].approvedProfiles.push(username);
+      if (!newUserData[newUserData[username].matchmaker].approvedProfiles.includes(newUserData[props.username].matchmaking)) {
+        newUserData[newUserData[username].matchmaker].mProfiles.push(newUserData[props.username].matchmaking);
+      }
+    }
+    console.log(newUserData);
     props.setUserData(newUserData);
     setProfiles(newUserData[props.username].mProfiles);
   };
