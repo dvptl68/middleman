6aa621adafd0d401b84abed8a825c9dba0642845
Dev Patel <dvptl68@gmail.com>
2022-09-28 17:27:07 -0400

    Add chat functionality

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { ChatStyles } from '../styles/Styles';
 
-const Chat = () => {
+const Chat = (props) => {
+  const chatUsernames = []
+  const userLiked = props.userData[props.username].userLiked;
+  userLiked.forEach(username => {
+    const otherUserLiked = props.userData[username].userLiked;
+    if (otherUserLiked.includes(props.username)) {
+      chatUsernames.push(username)
+    }
+  });
+  console.log(chatUsernames)
   return (
     <View style={ChatStyles.container}>
       <Text>Chat</Text>
