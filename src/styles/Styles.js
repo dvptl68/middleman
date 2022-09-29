944055f54b1dfd654f94d714377adb777c436799
smerapala <smerapala@gmail.com>
2022-09-29 13:11:52 -0400

    Fix linter stuff

--- src/styles/Styles.js
+++ src/styles/Styles.js
@@ -3,7 +3,7 @@ import Colors from './Colors';
 
 const showContainerBorders = false;
 const containerBorderWidth = 4;
-const defaultFont = "sans-serif";
+const defaultFont = 'sans-serif';
 
 const LoginStyles = StyleSheet.create({
   baseText: {
