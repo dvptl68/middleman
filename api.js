a36e8e603b1429829f5f68100632545719899f57
Dev Patel <dvptl68@gmail.com>
2022-10-16 23:30:54 -0400

    implement majority of replacement api

--- api.js
+++ api.js
@@ -7,52 +7,110 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded());
 
-const data = {};
+const data = {
+  // 'superman': {
+  //   'matchmaker': 'batman',
+  //   'matchmaking': 'batman',
+  //   'mProfiles': [],
+  //   'approvedProfiles': [],
+  //   'userLiked': [],
+  // },
+  // 'batman': {
+  //   'matchmaker': 'superman',
+  //   'matchmaking': 'superman',
+  //   'mProfiles': [],
+  //   'approvedProfiles': [],
+  //   'userLiked': [],
+  // },
+  // 'wonderwoman': {
+  //   'matchmaker': 'catwoman',
+  //   'matchmaking': 'catwoman',
+  //   'mProfiles': [],
+  //   'approvedProfiles': [],
+  //   'userLiked': [],
+  // },
+  // 'catwoman': {
+  //   'matchmaker': 'wonderwoman',
+  //   'matchmaking': 'wonderwoman',
+  //   'mProfiles': [],
+  //   'approvedProfiles': [],
+  //   'userLiked': [],
+  // }
+};
 
 // Adds new profile to the data
 const initProfile = (username, matchmaker) => {
   if (!(username in data)) {
     data[username] = {
-      "matchmaker": matchmaker,
-      "matchmaking": matchmaker,
-      "mProfiles": [],
-      "approvedProfiles": [],
-      "userLiked": [],
+      'matchmaker': matchmaker,
+      'matchmaking': matchmaker,
+      'mProfiles': [],
+      'approvedProfiles': [],
+      'userLiked': [],
     }
   }
   if (!(matchmaker in data)) {
     data[matchmaker] = {
-      "matchmaker": username,
-      "matchmaking": username,
-      "mProfiles": [],
-      "approvedProfiles": [],
-      "userLiked": [],
+      'matchmaker': username,
+      'matchmaking': username,
+      'mProfiles': [],
+      'approvedProfiles': [],
+      'userLiked': [],
     }
   }
 };
 
 app.post('/matchmaker_profiles', (req, res) => {
   const username = req.body.username;
-  const matchmaker = req.body.matchmaker;
-  initProfile(username, matchmaker);
-  res.send(data[username]["mProfiles"]);
+  const matchmaking = req.body.matchmaking;
+  initProfile(username, matchmaking);
+  res.send(data[username]['mProfiles']);
 });
 
 app.post('/matchmaker_like', (req, res) => {
   const username = req.body.username;
+  const matchmaking = data[username]['matchmaking'];
   const likedUsername = req.body.likedUsername;
   const likedUsernameMatchmaker = req.body.likedUsernameMatchmaker;
-  const liked = req.body.liked;
   initProfile(likedUsername, likedUsernameMatchmaker);
-  data[username]["mProfiles"] = data[username]["mProfiles"].filter(mUsername => mUsername !== likedUsername);
-  if (liked) {
-    data[username]["approvedProfiles"].push(likedUsername);
-    if (!data[data[likedUsername]["matchmaker"]]["approvedProfiles"].includes(data[username]["matchmaking"])) {
-      console.log("adding to other matchmaker's mProfiles")
-      data[data[likedUsername]["matchmaker"]]["mProfiles"].push(data[username]["matchmaking"])
+  data[username]['mProfiles'] = data[username]['mProfiles'].filter(mUser => mUser !== likedUsername);
+  if (req.body.liked) {
+    data[username]['approvedProfiles'].push(likedUsername);
+    if (!data[likedUsernameMatchmaker]['approvedProfiles'].includes(matchmaking)) {
+      data[likedUsernameMatchmaker]['mProfiles'].push(matchmaking)
     }
+  } else {
+    data[likedUsernameMatchmaker]['approvedProfiles'] = data[likedUsernameMatchmaker]['approvedProfiles'].filter(approvedUser => approvedUser !== matchmaking)
   }
   res.send(data);
 });
 
-app.listen(port, () => console.log(`Listening at port ${port}`));
+app.post('/user_profiles', (req, res) => {
+  const username = req.body.username;
+  const matchmaker = req.body.matchmaker;
+  const userProfiles = []
+  for (approvedUser of data[matchmaker]['approvedProfiles']) {
+    const approvedUserMatchmaker = data[approvedUser]['matchmaker']
+    if (data[approvedUserMatchmaker]['approvedProfiles'].includes(username) && !data[username]['userLiked'].includes(approvedUser)) {
+      userProfiles.push(approvedUser);
+    }
+  }
+  res.send(userProfiles)
+});
+
+app.post('/user_like', (req, res) => {
+  const username = req.body.username;
+  const matchmaker = data[username]['matchmaker'];
+  const likedUsername = req.body.likedUsername;
+  const likedUsernameMatchmaker = data[likedUsername]['matchmaker'];
+  if (req.body.liked) {
+    data[username]['userLiked'].push(likedUsername)
+  } else {
+    data[likedUsername]['userLiked'] = data[likedUsername]['userLiked'].filter(likedUser => likedUser !== username)
+    data[matchmaker]['approvedProfiles'] = data[matchmaker]['approvedProfiles'].filter(approvedUser => approvedUser !== likedUsername);
+    data[likedUsernameMatchmaker]['approvedProfiles'] = data[likedUsernameMatchmaker]['approvedProfiles'].filter(approvedUser => approvedUser !== username);
+  }
+  res.send(data)
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/pages/Matchmaking.js
+++ src/pages/Matchmaking.js
@@ -4,43 +4,22 @@ import DisplayProfile from '../components/DisplayProfile';
 import { MatchmakingStyles } from '../styles/Styles';
 
 const Matchmaking = (props) => {
-  const [profiles, setProfiles] = useState([
-    ...props.userData[props.username].mProfiles,
-  ]);
+  console.log(props)
+  const [profiles, setProfiles] = useState([]);
+  useEffect(() => {
+    // make api call to set initial matchmaker profiles
+    setProfiles([]);
+  }, []);
   const likeProfile = (liked) => {
-    const username = profiles[0];
-    const newUserData = { ...props.userData };
-    newUserData[props.username].mProfiles = profiles.slice(1);
-    if (liked) {
-      newUserData[props.username].approvedProfiles.push(username);
-      if (
-        !newUserData[
-          newUserData[username].matchmaker
-        ].approvedProfiles.includes(newUserData[props.username].matchmaking)
-      ) {
-        newUserData[newUserData[username].matchmaker].mProfiles.push(
-          newUserData[props.username].matchmaking
-        );
-      }
-    }
-    props.setUserData(newUserData);
-    setProfiles(newUserData[props.username].mProfiles);
+    // make api call to like/dislike profile
+    setProfiles(profiles.slice(1));
   };
-  let username = '';
-  let params = null;
-  if (typeof profiles[0] == 'string') {
-    username = profiles[0];
-    params = props.userData[username].profile;
-  } else {
-    params = profiles[0];
-  }
   return profiles.length > 0 ? (
     <View style={MatchmakingStyles.container}>
-      <DisplayProfile
+      {/* <DisplayProfile
         username={username}
         likeProfile={likeProfile}
-        {...params}
-      />
+      /> */}
     </View>
   ) : (
     <View style={MatchmakingStyles.noProfilesTextContainer}>
