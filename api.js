0c58a520a8aa96a3aeb724b5f222686e61a9441c
Dev Patel <dvptl68@gmail.com>
2022-10-17 20:48:03 -0400

    comment code

--- api.js
+++ api.js
@@ -60,6 +60,7 @@ const initProfile = (username, matchmaker) => {
   }
 };
 
+// Returns a list of profiles that {matchmaker} needs to see
 app.post('/matchmaker_profiles', (req, res) => {
   const username = req.body.username;
   const matchmaking = req.body.matchmaking;
@@ -67,30 +68,38 @@ app.post('/matchmaker_profiles', (req, res) => {
   res.send(data[username]['mProfiles']);
 });
 
+// Likes/dislikes {userB} from {matchmakerA}
 app.post('/matchmaker_like', (req, res) => {
   const username = req.body.username;
   const matchmaking = data[username]['matchmaking'];
   const likedUsername = req.body.likedUsername;
   const likedUsernameMatchmaker = req.body.likedUsernameMatchmaker;
   initProfile(likedUsername, likedUsernameMatchmaker);
+  // Remove likedUsername from matchmaker's mProfiles
   data[username]['mProfiles'] = data[username]['mProfiles'].filter(mUser => mUser !== likedUsername);
   if (req.body.liked) {
+    // Add to approved profiles
     data[username]['approvedProfiles'].push(likedUsername);
+    // Add to other matchmaker's mProfiles if it's not in their approvedProfiles
     if (!data[likedUsernameMatchmaker]['approvedProfiles'].includes(matchmaking)) {
       data[likedUsernameMatchmaker]['mProfiles'].push(matchmaking)
     }
   } else {
+    // Remove disliked user from other matchmaker's approvedProfiles if it exists
     data[likedUsernameMatchmaker]['approvedProfiles'] = data[likedUsernameMatchmaker]['approvedProfiles'].filter(approvedUser => approvedUser !== matchmaking)
   }
   res.send(data);
 });
 
+// Returns a list of profiles that {username} needs to see
 app.post('/user_profiles', (req, res) => {
   const username = req.body.username;
   const matchmaker = req.body.matchmaker;
   const userProfiles = []
+  // Get every profile from matchmaker's approvedProfiles
   for (approvedUser of data[matchmaker]['approvedProfiles']) {
     const approvedUserMatchmaker = data[approvedUser]['matchmaker']
+    // Add approved user to list if user exists in their matchmaker's approvedProfiles
     if (data[approvedUserMatchmaker]['approvedProfiles'].includes(username) && !data[username]['userLiked'].includes(approvedUser)) {
       userProfiles.push(approvedUser);
     }
@@ -98,15 +107,19 @@ app.post('/user_profiles', (req, res) => {
   res.send(userProfiles)
 });
 
+// Likes/dislikes {userB} from {userA}
 app.post('/user_like', (req, res) => {
   const username = req.body.username;
   const matchmaker = data[username]['matchmaker'];
   const likedUsername = req.body.likedUsername;
   const likedUsernameMatchmaker = data[likedUsername]['matchmaker'];
   if (req.body.liked) {
+    // Add liked user to user's userLiked
     data[username]['userLiked'].push(likedUsername)
   } else {
+    // Remove user from disliked user's userLiked if it exists
     data[likedUsername]['userLiked'] = data[likedUsername]['userLiked'].filter(likedUser => likedUser !== username)
+    // Remove selves from each other's matchmakers approvedProfiles
     data[matchmaker]['approvedProfiles'] = data[matchmaker]['approvedProfiles'].filter(approvedUser => approvedUser !== likedUsername);
     data[likedUsernameMatchmaker]['approvedProfiles'] = data[likedUsernameMatchmaker]['approvedProfiles'].filter(approvedUser => approvedUser !== username);
   }
