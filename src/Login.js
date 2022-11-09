0c9f5c43f666d95e6419d3117f15f43c385421c4
smerapala <smerapala@gmail.com>
2022-11-09 16:30:11 -0500

    carried the pm review

--- src/Login.js
+++ src/Login.js
@@ -8,20 +8,25 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState();
+  const regex = new RegExp("^[a-zA-Z0-9_]*$");
   const getUserProfile = () => {
-    const requestOptions = {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    };
-    fetch(`http://127.0.0.1:5000/get_user_detail/${username}/`, requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        const userProfile = data[0];
-        setUserData(userProfile);
-        console.log(userProfile);
-        setIsLoggedIn(true);
-      })
-      .catch(console.error);
+    if (!regex.test(username)) {
+      console.log("Invalid username");
+    } else {
+      const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      };
+      fetch(`http://127.0.0.1:5000/get_user_detail/${username}/`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          const userProfile = data[0];
+          setUserData(userProfile);
+          console.log(userProfile);
+          setIsLoggedIn(true);
+        })
+        .catch(console.error);
+    }
   };
   return isLoggedIn ? (
     <MainView {...userData} setLoggedIn={setIsLoggedIn} />
